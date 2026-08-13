9bd53148703d38d873b882b69eebf83fb323e380
KashishAnil <kashish.kumar@salsoft.net>
2026-08-13 11:49:26 +0500

    syncing cursor branch and main branch

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const app=express();
+const https = require("https");
+const http = require("http");
+const env = process.env.NODE_ENV || "development";
+const credentials = require('./ssl');
 
-app.use(cors())
+require("dotenv").config(
+  { path: `.env.${env}` }
+);
+const {NODE_ENV}=process.env
+
+app.use(cors()); //allowing this enables different websites/ports to talk to each other. backend and frontend live on different ports. By default, ports cannot talk with other ports. Only when you allow cors
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log('Connection error:', err));
@@ -16,6 +25,7 @@ app.use((req, res, next) => {
     express.json()(req, res, next);
   }
 });
+
 //mounting paths on server
 const productRoutes = require('./routes/products'); //imports the router object imported from ./routes/products. This means everything written in routes/products is in productRoutes. 
 app.use('/products', productRoutes); //adding this to the list of methods: function 
@@ -33,10 +43,15 @@ const orderRoutes = require('./routes/orders');
 app.use('/orders', orderRoutes);
 
 const paymentsRoutes = require('./routes/payments');
+
 app.use('/payments', paymentsRoutes);
 
 app.use(express.json());
 
-app.listen(process.env.PORT || 3000, () => {
+var httpsServer;
+if (NODE_ENV === "customdev") httpsServer = https.createServer(credentials, app);
+else httpsServer = http.createServer(app);
+
+httpsServer.listen(process.env.PORT || 3000, () => {
   console.log('Server is running');
 });
